8be6fc6f804aba22a1c0e0e44967a0355394d645
TrueID Pvt Ltd <TrueID Pvt Ltd@LAPTOP-QM9QRTNG>
2023-03-14 15:52:05 +0530

    nani made changes to branch 'nani1'

--- src/pack1/Demo2.java
+++ src/pack1/Demo2.java
@@ -5,6 +5,9 @@ public class Demo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Demo 2 code.....");
+		for(int i=1;i<10;i++)
+			System.out.println("HI"+i);
+		System.out.println("HI narendra");
 	}
 
 }
